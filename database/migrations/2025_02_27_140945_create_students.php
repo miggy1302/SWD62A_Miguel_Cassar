dc78d7b629851d669889fa57b4540fb898b15ea0
miggy1302 <miggycass@gmail.com>
2025-02-27 14:13:01 +0000

    completed migrations

---
+++ database/migrations/2025_02_27_140945_create_students.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->notNullable();
+            $table->string('email')->unique()->notNullable();
+            $table->string('phone')->notNullable();
+            $table->date('dob')->notNullable();
+            $table->foreignId('college_id')->constrained('colleges')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
