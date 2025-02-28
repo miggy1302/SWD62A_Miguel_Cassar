e7c7291ef4417f5e51b66b9aadfd90d0e40ef9ef
miggy1302 <miggycass@gmail.com>
2025-02-28 15:47:39 +0000

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('college.index');
 });
+
+//-----------COLLEGES ROUTES---------------
+
+// route that will return a list of Colleges
+Route::get('/colleges', [CollegeController::class, 'index'])->name('colleges.index');
+
+// route that will allow a user to create a College
+Route::get('/colleges/create', [CollegeController::class, 'create'])->name('colleges.create');
+
+// Route that will store the data of a new College
+Route::post('/colleges', [CollegeController::class, 'store'])->name('colleges.store');
+
+// route that will show the edit form
+Route::get('/colleges/{id}/edit', [CollegeController::class, 'edit'])->name('colleges.edit');
+
+//route that will process the updated values of the College
+Route::put('/colleges/{id}', [CollegeController::class, 'update'])->name('colleges.update');
+
+//-----------STUDENTS ROUTES---------------
+
+// route that will return a list of students
+Route::get('/students', [StudentController::class, 'index'])->name('colleges.index');
+
+// route that will allow a user to create a students
+Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+
+// Route that will store the data of a new students
+Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+
+// route that will show the edit form
+Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+
+//route that will process the updated values of the students
+Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
+
+// Route that will delete a specific students
+Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
