fdf1cacadff190a71812b5959029a7ff5d6cc3d3
miggy1302 <miggycass@gmail.com>
2025-02-28 15:58:35 +0000

    finished college controller

--- app/Http/Controllers/CollegeController.php
+++ app/Http/Controllers/CollegeController.php
@@ -7,5 +7,31 @@ use App\Models\College;
 
 class CollegeController extends Controller
 {
-    //
+    //display all colleges
+    public function index(){
+        
+        $colleges = College::all();   
+
+        return view('colleges.index', compact('colleges'));
+    }
+
+    //create new college
+    public function create(){
+    
+        $collage = new collage();
+        return view('collages.create', compact('collage'));
+    }
+
+    //store the form data
+    public function store(Request $request){
+        $request -> validate([
+            'name' => 'required',
+            'address' => 'required'
+        ]);
+
+        Collage::create($request->all());
+
+        return redirect()->route('collages.index')->with('message', 'collage has been added successfully');
+    }
+    
 }
